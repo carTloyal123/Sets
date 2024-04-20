27bbcfe9b6fb36c3047a0442cdd24f44a97b0b5e
Carson Loyal <carsonloyal123@me.com>
2024-04-20 11:30:24 -0500

    Initial Commit

---
+++ Sets Watch App/SetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetsApp.swift
+//  Sets Watch App
+//
+//  Created by Carson Loyal on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Sets_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
