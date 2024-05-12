e2e1b04dabaa11f1da9ebde07a872b1f9ad20a36
Carson Loyal <carsonloyal123@me.com>
2024-05-12 16:07:02 -0500

    SET-24 Fix crash when scheduling set notification timer

--- Sets Watch App/SetsApp.swift
+++ Sets Watch App/SetsApp.swift
@@ -13,15 +13,13 @@ import UserNotifications
 struct Sets_Watch_AppApp: App {    
     // This is the main entry for the app
     @State var settings_controller: SettingsController = SettingsController()
-    @State var current_workout: Workout = ExampleData().GetExampleStrengthWorkout()
-    @State var app_storage: CentralStorage = ExampleData().GetExampleAppStorage()
+    @State var app_storage: CentralStorage = CentralStorage()
     @State var fitness_db: FitnessDatabase = ExampleData().GenerateExampleFitnessDatabase()
     
     var body: some Scene {
         WindowGroup {
             EntryLayerView()
                 .environmentObject(settings_controller)
-                .environment(current_workout)
                 .environment(app_storage)
                 .environment(fitness_db)
         }
